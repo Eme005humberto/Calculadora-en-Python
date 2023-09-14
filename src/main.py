a420ffb8c014ae32529593e1b7f6798c63298557
Emerson Humberto <emersoncarpano3@gmail.com>
2023-09-14 11:05:18 -0600

    Segundo Commit

--- src/main.py
+++ src/main.py
@@ -1,1 +1,9 @@
-fin = False
+fin = False #Variable that stores a value of false
+print("""=== CALCULADORA ===
+      \nMenu Principal -> Elija Una opcion: 
+      \n1 Sumar
+      \n2 Restar
+      \n3 Multiplicar
+      \n4 Dividir
+      \n5 Salir
+      """)
