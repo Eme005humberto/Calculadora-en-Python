bef3633dd24cf3ec85b65f2f4e16750bc4d82cf9
Emerson Humberto <emersoncarpano3@gmail.com>
2023-09-15 14:32:14 -0600

    Cuarto Commit

--- src/main.py
+++ src/main.py
@@ -6,7 +6,8 @@ print("""=== CALCULADORA ===
       \n3 Multiplicar
       \n4 Dividir
       \n5 Salir
-      """)#Agregamos un blucle while que se ejecutara multiples veces al menos que nosotros salgamos del
+      """)
+        #Agregamos un blucle while que se ejecutara multiples veces al menos que nosotros salgamos del
         #programa
         while not(fin):
             opc = int(input("Opcion: "))#declaramos una variable Opc que contiene una entrada de datos
@@ -17,3 +18,17 @@ print("""=== CALCULADORA ===
                 sum1 = int(input("Ingrese el primer numero: ")) 
                 sum2 = int(input("Ingrese el segundo numero: ")) 
                 print("La suma es de: ",sum1 + sum2)
+            elif(opc == 2):
+              rest1 = int(input("Ingrese el primer nunero: "))
+              rest2 = int(input("Ingrese el segundo numero: "))
+              print("La Resta es de: ",rest1-rest2)
+            elif(opc == 3):
+              product1 = int(input("Ingrese el primer numero: "))
+              product2 = int(input("Ingrese el segundo numero: "))
+              print("El producto es de: ",product1 * product2)
+            elif(opc == 4):
+              dividir1 = int(input("Ingrese el primer numero: "))
+              dividir2 = int(input("Ingrese el segundo numero: "))
+              print("La division es de: ",dividir1/dividir2)
+            elif(opc == 5):
+              fin = True
